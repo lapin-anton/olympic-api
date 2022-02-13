9c1cd2afbd3351a0bc160a814f43584a6a499381
AntonLapin <lapinlisss@gmail.com>
2022-02-13 13:16:06 +0300

    init commit

---
+++ src/main/java/ru/lapinlisss/olympic_api/OlympicApiApplication.java
@@ -0,0 +1,13 @@
+package ru.lapinlisss.olympic_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlympicApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lapinlisss/olympic_api/OlympicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lapinlisss.olympic_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
