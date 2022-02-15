659f7701a88940da7dc0f292d4c1357fa017981a
anton-lapin <lapin_antoine@mail.ru>
2022-02-15 17:55:49 +0300

    add athlete controller

---
+++ src/main/java/ru/lapinlisss/olympic_api/controller/AthleteController.java
@@ -0,0 +1,26 @@
+package ru.lapinlisss.olympic_api.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("athlete")
+@RequiredArgsConstructor
+public class AthleteController {
+
+    // get athlete by id
+
+    // get athlete by name
+
+    // get athletes by country
+
+    // get athletes by country and game
+
+    // get athletes by country, game and sport
+
+    // ...
+
+}
