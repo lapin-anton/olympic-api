f2723c9b8f0190c2b688381c187aebbd8cab5dc5
anton-lapin <lapin_antoine@mail.ru>
2022-02-26 21:14:27 +0300

    add styles for app bar

---
+++ ui/olympic-ui/src/css-common.js
@@ -0,0 +1,11 @@
+import {blue, deepPurple, green, grey, red, yellow} from '@mui/material/colors';
+
+export const styles = {
+    link: {
+        textTransform: "unset",
+        color: "#fff",
+        margin: "0 10px",
+        textDecoration: "unset"
+    }
+}
+
