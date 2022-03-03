6126eda15b135547a89f9fca90b16d5121d6212e
anton-lapin <lapin_antoine@mail.ru>
2022-03-03 21:08:52 +0300

    refactoring

---
+++ ui/olympic-ui/src/components/team-rating-table/team-rating-table.js
@@ -0,0 +1,131 @@
+import React, {Component} from 'react';
+
+import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
+
+import {withStyles} from '@mui/styles';
+import {styles} from "../../css-common";
+import {
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableFooter,
+    TableHead,
+    TablePagination,
+    TableRow
+} from "@mui/material";
+
+class TeamRatingTable extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: 0,
+            rowsPerPage: 5
+        }
+    }
+
+    handleChangePage = (event, newPage) => {
+        this.setState({
+            page: newPage
+        });
+    };
+
+    handleChangeRowsPerPage = (event) => {
+        this.setState({
+            rowsPerPage: parseInt(event.target.value, 10),
+            page: 0
+        });
+    };
+
+    render() {
+
+        const {classes, rating} = this.props;
+
+        const {page, rowsPerPage} = this.state;
+
+        const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rating.length) : 0;
+
+        return (
+            <>
+                <h2 className={classes.listHead}>Unofficial Team Medal Count</h2>
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow className={classes.headRow}>
+                                <TableCell align="center">Place</TableCell>
+                                <TableCell align="center">Country's Team</TableCell>
+                                <TableCell align="center">Gold</TableCell>
+                                <TableCell align="center">Silver</TableCell>
+                                <TableCell align="center">Bronze</TableCell>
+                                <TableCell align="center">Total</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {(rowsPerPage > 0
+                                    ? rating.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    : rating
+                            ).map((r, i) => {
+
+                                const place = page * rowsPerPage + i + 1;
+
+                                let rowStyle = classes.commonRowColor;
+
+                                if (place === 1) {
+                                    rowStyle = classes.goldColor;
+                                }
+
+                                if (place === 2) {
+                                    rowStyle = classes.silverColor;
+                                }
+
+                                if (place === 3) {
+                                    rowStyle = classes.bronzeColor;
+                                }
+
+                                return (<TableRow className={rowStyle}>
+                                    <TableCell align="center">{place}</TableCell>
+                                    <TableCell align="center">{r.country}</TableCell>
+                                    <TableCell align="center">{r.gold}</TableCell>
+                                    <TableCell align="center">{r.silver}</TableCell>
+                                    <TableCell align="center">{r.bronze}</TableCell>
+                                    <TableCell align="center">{r.total}</TableCell>
+                                </TableRow>)
+                            })}
+
+                            {emptyRows > 0 && (
+                                <TableRow style={{ height: 53 * emptyRows }}>
+                                    <TableCell colSpan={6} />
+                                </TableRow>
+                            )}
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TablePagination
+                                    rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                                    colSpan={3}
+                                    count={rating.length}
+                                    rowsPerPage={rowsPerPage}
+                                    page={page}
+                                    SelectProps={{
+                                        inputProps: {
+                                            'aria-label': 'rows per page',
+                                        },
+                                        native: true,
+                                    }}
+                                    onPageChange={this.handleChangePage}
+                                    onRowsPerPageChange={this.handleChangeRowsPerPage}
+                                    ActionsComponent={TablePaginationActions}
+                                />
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </TableContainer>
+            </>
+        );
+    }
+
+}
+
+export default withStyles(styles) (TeamRatingTable);
--- ui/olympic-ui/src/pages/games-page/game-page.js
+++ ui/olympic-ui/src/pages/games-page/game-page.js
@@ -4,13 +4,13 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
-import {Container, Grid, Paper, TableFooter, TableHead, TablePagination} from "@mui/material";
+import {Container, Grid, Paper} from "@mui/material";
 import {withStyles} from '@mui/styles';
 import {styles} from "../../css-common";
 
 import GameService from '../../services/game-service';
 import ResultsService from "../../services/results-service";
-import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
+import TeamRatingTable from "../../components/team-rating-table/team-rating-table";
 
 
 class GamePage extends Component {
@@ -20,9 +20,7 @@ class GamePage extends Component {
             game: null,
             rating: [],
             athleteCount: 0,
-            sportCount: 0,
-            rowsPerPage: 5,
-            page: 0
+            sportCount: 0
         }
     }
 
@@ -52,26 +50,11 @@ class GamePage extends Component {
             .catch(e => console.log(e));
     }
 
-    handleChangePage = (event, newPage) => {
-        this.setState({
-            page: newPage
-        });
-    };
-
-    handleChangeRowsPerPage = (event) => {
-        this.setState({
-            rowsPerPage: parseInt(event.target.value, 10),
-            page: 0
-        });
-    };
-
     render() {
 
-        const {game, rating, athleteCount, sportCount, rowsPerPage, page} = this.state;
+        const {game, rating, athleteCount, sportCount} = this.state;
 
         const {classes} = this.props;
-
-        const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rating.length) : 0;
 
         return (
             <Container maxWidth="lg">
@@ -144,79 +127,7 @@ class GamePage extends Component {
                 </Grid>
                 <Grid container spacing={1}>
                     <Grid item xs={12}>
-                        <h2 className={classes.listHead}>Unofficial Team Medal Count</h2>
-                        <TableContainer component={Paper}>
-                            <Table aria-label="simple table">
-                                <TableHead>
-                                    <TableRow className={classes.headRow}>
-                                        <TableCell align="center">Place</TableCell>
-                                        <TableCell align="center">Country's Team</TableCell>
-                                        <TableCell align="center">Gold</TableCell>
-                                        <TableCell align="center">Silver</TableCell>
-                                        <TableCell align="center">Bronze</TableCell>
-                                        <TableCell align="center">Total</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {(rowsPerPage > 0
-                                            ? rating.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                            : rating
-                                    ).map((r, i) => {
-
-                                        const place = page * rowsPerPage + i + 1;
-
-                                        let rowStyle = classes.commonRowColor;
-
-                                        if (place === 1) {
-                                            rowStyle = classes.goldColor;
-                                        }
-
-                                        if (place === 2) {
-                                            rowStyle = classes.silverColor;
-                                        }
-
-                                        if (place === 3) {
-                                            rowStyle = classes.bronzeColor;
-                                        }
-
-                                        return (<TableRow className={rowStyle}>
-                                            <TableCell align="center">{place}</TableCell>
-                                            <TableCell align="center">{r.country}</TableCell>
-                                            <TableCell align="center">{r.gold}</TableCell>
-                                            <TableCell align="center">{r.silver}</TableCell>
-                                            <TableCell align="center">{r.bronze}</TableCell>
-                                            <TableCell align="center">{r.total}</TableCell>
-                                        </TableRow>)
-                                    })}
-
-                                    {emptyRows > 0 && (
-                                        <TableRow style={{ height: 53 * emptyRows }}>
-                                            <TableCell colSpan={6} />
-                                        </TableRow>
-                                    )}
-                                </TableBody>
-                                <TableFooter>
-                                    <TableRow>
-                                        <TablePagination
-                                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                                            colSpan={3}
-                                            count={rating.length}
-                                            rowsPerPage={rowsPerPage}
-                                            page={page}
-                                            SelectProps={{
-                                                inputProps: {
-                                                    'aria-label': 'rows per page',
-                                                },
-                                                native: true,
-                                            }}
-                                            onPageChange={this.handleChangePage}
-                                            onRowsPerPageChange={this.handleChangeRowsPerPage}
-                                            ActionsComponent={TablePaginationActions}
-                                        />
-                                    </TableRow>
-                                </TableFooter>
-                            </Table>
-                        </TableContainer>
+                        <TeamRatingTable rating={rating} />
                     </Grid>
                 </Grid>
             </Container>
