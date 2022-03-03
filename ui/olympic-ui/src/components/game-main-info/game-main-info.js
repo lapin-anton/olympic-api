5ec1ef3bf584fee4242d1a129bf24272ef187e00
anton-lapin <lapin_antoine@mail.ru>
2022-03-03 21:20:39 +0300

    refactoring

---
+++ ui/olympic-ui/src/components/game-main-info/game-main-info.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+import {withStyles} from '@mui/styles';
+import {styles} from "../../css-common";
+import {Paper} from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+
+function GameMainInfo(props) {
+
+    const {classes, game, rating, athleteCount, sportCount} = props;
+
+    return (
+        <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+                <TableBody>
+                    <TableRow
+                        key={"city"}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        className={classes.oddRow}
+                    >
+                        <TableCell component="th" scope="row">City</TableCell>
+                        <TableCell align="center">{game && game.city}</TableCell>
+                    </TableRow>
+                    <TableRow
+                        key={"year"}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        className={classes.evenRow}
+                    >
+                        <TableCell component="th" scope="row">Year</TableCell>
+                        <TableCell align="center">{game && game.year}</TableCell>
+                    </TableRow>
+                    <TableRow
+                        key={"type"}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        className={classes.oddRow}
+                    >
+                        <TableCell component="th" scope="row">Season</TableCell>
+                        <TableCell align="center">{game && game.type}</TableCell>
+                    </TableRow>
+                    <TableRow
+                        key={"type"}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        className={classes.evenRow}
+                    >
+                        <TableCell component="th" scope="row">Country's Teams</TableCell>
+                        <TableCell align="center">{rating && rating.length}</TableCell>
+                    </TableRow>
+                    <TableRow
+                        key={"type"}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        className={classes.oddRow}
+                    >
+                        <TableCell component="th" scope="row">Athletes</TableCell>
+                        <TableCell align="center">{athleteCount}</TableCell>
+                    </TableRow>
+                    <TableRow
+                        key={"type"}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        className={classes.evenRow}
+                    >
+                        <TableCell component="th" scope="row">Sports</TableCell>
+                        <TableCell align="center">{sportCount}</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+
+}
+
+export default withStyles(styles)(GameMainInfo);
--- ui/olympic-ui/src/pages/games-page/game-page.js
+++ ui/olympic-ui/src/pages/games-page/game-page.js
@@ -1,16 +1,14 @@
 import React, {Component} from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableRow from '@mui/material/TableRow';
-import {Container, Grid, Paper} from "@mui/material";
+
+import TeamRatingTable from "../../components/team-rating-table/team-rating-table";
+import GameMainInfo from "../../components/game-main-info/game-main-info";
+
+import {Container, Grid} from "@mui/material";
 import {withStyles} from '@mui/styles';
 import {styles} from "../../css-common";
 
 import GameService from '../../services/game-service';
 import ResultsService from "../../services/results-service";
-import TeamRatingTable from "../../components/team-rating-table/team-rating-table";
 
 
 class GamePage extends Component {
@@ -54,8 +52,6 @@ class GamePage extends Component {
 
         const {game, rating, athleteCount, sportCount} = this.state;
 
-        const {classes} = this.props;
-
         return (
             <Container maxWidth="lg">
                 <h1 style={{"text-transform":"uppercase", "text-align":"center", "margin-top": "80px"}}>Game Info</h1>
@@ -69,60 +65,12 @@ class GamePage extends Component {
                         </div>
                     </Grid>
                     <Grid item xs={6}>
-                            <TableContainer component={Paper}>
-                                <Table aria-label="simple table">
-                                    <TableBody>
-                                        <TableRow
-                                            key={"city"}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                            className={classes.oddRow}
-                                        >
-                                            <TableCell component="th" scope="row">City</TableCell>
-                                            <TableCell align="center">{game && game.city}</TableCell>
-                                        </TableRow>
-                                        <TableRow
-                                            key={"year"}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                            className={classes.evenRow}
-                                        >
-                                            <TableCell component="th" scope="row">Year</TableCell>
-                                            <TableCell align="center">{game && game.year}</TableCell>
-                                        </TableRow>
-                                        <TableRow
-                                            key={"type"}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                            className={classes.oddRow}
-                                        >
-                                            <TableCell component="th" scope="row">Season</TableCell>
-                                            <TableCell align="center">{game && game.type}</TableCell>
-                                        </TableRow>
-                                        <TableRow
-                                            key={"type"}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                            className={classes.evenRow}
-                                        >
-                                            <TableCell component="th" scope="row">Country's Teams</TableCell>
-                                            <TableCell align="center">{rating && rating.length}</TableCell>
-                                        </TableRow>
-                                        <TableRow
-                                            key={"type"}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                            className={classes.oddRow}
-                                        >
-                                            <TableCell component="th" scope="row">Athletes</TableCell>
-                                            <TableCell align="center">{athleteCount}</TableCell>
-                                        </TableRow>
-                                        <TableRow
-                                            key={"type"}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                            className={classes.evenRow}
-                                        >
-                                            <TableCell component="th" scope="row">Sports</TableCell>
-                                            <TableCell align="center">{sportCount}</TableCell>
-                                        </TableRow>
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
+                         <GameMainInfo
+                             game={game}
+                             rating={rating}
+                             athleteCount={athleteCount}
+                             sportCount={sportCount}
+                         />
                     </Grid>
                 </Grid>
                 <Grid container spacing={1}>
